1b52c51e4a5da1cd61b859c32ea0ac54ea3ce7a1
guoqiang <guoqiang.zhu@ubixai.com>
2024-06-21 10:53:39 +0800

    update 1.0.1

---
+++ UbiXMTopOnAdapter/UbiXMTopOnAdapter.framework/Headers/UbiXMTopOnAdapter-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "UbiXMTopOnAdapter.h"
+
+FOUNDATION_EXPORT double UbiXMTopOnAdapterVersionNumber;
+FOUNDATION_EXPORT const unsigned char UbiXMTopOnAdapterVersionString[];
+
